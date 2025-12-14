1507569a4ba1c4357ad3635ef30656a53833442d
Levi Han <levi.h@binance.com>
2025-12-14 16:37:13 +0400

    fix UI missing after App enter backgroud

--- Sources/Internal/Manager/CameraManager+NotificationCenter.swift
+++ Sources/Internal/Manager/CameraManager+NotificationCenter.swift
@@ -10,6 +10,7 @@
 
 
 import Foundation
+import UIKit
 
 @MainActor class CameraManagerNotificationCenter {
     private(set) var parent: CameraManager!
@@ -20,6 +21,8 @@ extension CameraManagerNotificationCenter {
     func setup(parent: CameraManager) {
         self.parent = parent
         NotificationCenter.default.addObserver(self, selector: #selector(handleSessionWasInterrupted), name: .AVCaptureSessionWasInterrupted, object: parent.captureSession)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleAppDidBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleAppWillResignActive), name: UIApplication.willResignActiveNotification, object: nil)
     }
 }
 private extension CameraManagerNotificationCenter {
@@ -27,11 +30,22 @@ private extension CameraManagerNotificationCenter {
         parent.attributes.lightMode = .off
         parent.videoOutput.reset()
     }
+    @objc func handleAppDidBecomeActive() {
+        // 通知 SwiftUI 刷新视图（关键！）
+        // 因为 captureSession.isRunning 不是 @Published 属性，
+        // SwiftUI 不知道需要重新渲染依赖它的视图
+        parent.objectWillChange.send()
+    }
+    @objc func handleAppWillResignActive() {
+        // App 进入后台时，可以在这里做清理工作（可选）
+    }
 }
 
 // MARK: Reset
 extension CameraManagerNotificationCenter {
     func reset() {
         NotificationCenter.default.removeObserver(self, name: .AVCaptureSessionWasInterrupted, object: parent?.captureSession)
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didBecomeActiveNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIApplication.willResignActiveNotification, object: nil)
     }
 }
